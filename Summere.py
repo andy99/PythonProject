9494e8a475da8dfb5f4c84c564c0b52cf9cab4bd
Gunnar Nagell Andresen <52815785+andy99@users.noreply.github.com>
2022-09-25 12:02:14 +0200

    Add files via upload

---
+++ Summere.py
@@ -0,0 +1,10 @@
+
+def summere(tall):
+    print(tall,"Tall har verdi")
+    while tall < 6:
+        tall = tall + tall
+        print(tall,"tall har verdi")
+
+Antallinn = input("Tast et tall: " )
+tall = int(Antallinn)
+summere(tall)
